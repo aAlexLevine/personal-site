43681eb52f5eb9da1c8fd32a36a7d922e1fcf7ba
Alex Levine <alexlevine@Alexs-MBP-2.home>
2019-05-19 18:10:41 -0400

    Updates

---
+++ precache-manifest.5a159e60026e6c178c9459d0446dc405.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6978a064af961ea80078952737e1abfb",
+    "url": "/personal-site/index.html"
+  },
+  {
+    "revision": "6928bb3f55ff59166afa",
+    "url": "/personal-site/static/css/2.7abef457.chunk.css"
+  },
+  {
+    "revision": "a8548ca3a8def8a06cd1",
+    "url": "/personal-site/static/css/main.c70ff283.chunk.css"
+  },
+  {
+    "revision": "6928bb3f55ff59166afa",
+    "url": "/personal-site/static/js/2.d239f06e.chunk.js"
+  },
+  {
+    "revision": "a8548ca3a8def8a06cd1",
+    "url": "/personal-site/static/js/main.033325c5.chunk.js"
+  },
+  {
+    "revision": "d33cc63b615d83e32684",
+    "url": "/personal-site/static/js/runtime~main.039145d2.js"
+  },
+  {
+    "revision": "5a0316fdb23a959002293b6fbd49eeb9",
+    "url": "/personal-site/static/media/Lift Log Sign-In.5a0316fd.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-site/precache-manifest.1d464976b92e3ff12d275917879e1753.js"
+  "/personal-site/precache-manifest.5a159e60026e6c178c9459d0446dc405.js"
 );
 
 self.addEventListener('message', (event) => {
