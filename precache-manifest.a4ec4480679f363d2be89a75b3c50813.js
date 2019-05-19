db143234048a0fe22dff81c054ac6233f353cedd
Alex Levine <alexlevine@Alexs-MBP-2.home>
2019-05-19 17:47:16 -0400

    Updates

---
+++ precache-manifest.a4ec4480679f363d2be89a75b3c50813.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96fe810768df8e7bdadb4fc611f71868",
+    "url": "/personal-site/index.html"
+  },
+  {
+    "revision": "a69c65d008e4ade22342",
+    "url": "/personal-site/static/css/2.7abef457.chunk.css"
+  },
+  {
+    "revision": "957d8782d8fbb0b64292",
+    "url": "/personal-site/static/css/main.6530ce93.chunk.css"
+  },
+  {
+    "revision": "a69c65d008e4ade22342",
+    "url": "/personal-site/static/js/2.bf1bdc63.chunk.js"
+  },
+  {
+    "revision": "957d8782d8fbb0b64292",
+    "url": "/personal-site/static/js/main.c7791029.chunk.js"
+  },
+  {
+    "revision": "d33cc63b615d83e32684",
+    "url": "/personal-site/static/js/runtime~main.039145d2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aAlexLevine/personal-site/precache-manifest.475ef9bf690d0e32a896269b2784a676.js"
+  "/personal-site/precache-manifest.a4ec4480679f363d2be89a75b3c50813.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aAlexLevine/personal-site/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-site/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
